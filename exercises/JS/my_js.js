be92ee8be040e5cbb8eb940ab9b7d1560e36df18
Brandon Braun <brandon.braun@gmail.com>
2015-11-10 18:28:50 -0500

    complete context exercise

--- exercises/JS/my_js.js
+++ exercises/JS/my_js.js
@@ -1,11 +1,19 @@
 // 05 Context
 DOT = function(obj, prop){
-
+  if (obj === null)
+    return undefined;
+  if (obj.hasOwnProperty(prop))
+    return obj[prop];
+  return DOT(obj.__proto__, prop);
 }
 
 // 05 Context
 DOTCALL = function(obj, prop, args){
+  var propReturn = DOT(obj, prop);
 
+  if (typeof propReturn === 'function')
+    return propReturn.apply(obj, args);
+  return propReturn;
 }
 
 // 06 Prototypes
