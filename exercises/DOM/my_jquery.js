4d4096ad341dadbcd46bf62863eb439c05cd4418
Brandon Braun <brandon.braun@gmail.com>
2015-11-11 19:24:04 -0500

    complete multiple exercises up to proxy

--- exercises/DOM/my_jquery.js
+++ exercises/DOM/my_jquery.js
@@ -11,15 +11,44 @@
   };
 
   // Static methods
-  var isArrayLike = function(obj) {};
+  var isArrayLike = function(obj) {
+    if (typeof obj.length === 'number')
+      if (obj.length === 0 || (obj.length > 0 && obj.length - 1 in obj))
+        return true;
+    return false
+  };
 
   $.extend($, {
     isArray: function(obj) {
       return Object.prototype.toString.call(obj) === '[object Array]';
     },
-    each: function(collection, cb) {},
-    makeArray: function(arr) {},
-    proxy: function(fn, context) {}
+    each: function(collection, cb) {
+      if (isArrayLike(collection)) {
+        for (var i = 0; i < collection.length; i++) {
+          cb.call(collection[i], i, collection[i]);
+        }
+      }
+      else {
+        for (prop in collection) {
+          if (collection.hasOwnProperty(prop)) {
+            cb.call(collection[prop], prop, collection[prop]);
+          }
+        }
+      }
+      return collection;
+    },
+    makeArray: function(arr) {
+      var new_arr = [];
+      $.each(arr, function(index, val) {
+        new_arr.push(val);
+      });
+      return new_arr;
+    },
+    proxy: function(fn, context) {
+      return function() {
+        return fn.apply(context, arguments);
+      };
+    }
   });
 
   $.extend($.prototype, {
